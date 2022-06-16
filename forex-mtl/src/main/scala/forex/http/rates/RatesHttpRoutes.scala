3dfdd245e878b87e1e53970e285a320b4ac91d86
Leandro Galvan <leusgalvan@gmail.com>
2022-06-16 12:45:24 -0300

    Add error handling to rates http routes

--- forex-mtl/src/main/scala/forex/http/rates/RatesHttpRoutes.scala
+++ forex-mtl/src/main/scala/forex/http/rates/RatesHttpRoutes.scala
@@ -3,11 +3,15 @@ package rates
 
 import cats.effect.Sync
 import cats.syntax.flatMap._
+import cats.syntax.applicativeError._
 import forex.programs.RatesProgram
-import forex.programs.rates.{ Protocol => RatesProgramProtocol }
+import forex.programs.rates.errors.Error.RateLookupFailed
+import forex.programs.rates.{Protocol => RatesProgramProtocol}
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.Router
+
+import scala.util.control.NonFatal
 
 class RatesHttpRoutes[F[_]: Sync](rates: RatesProgram[F]) extends Http4sDsl[F] {
 
@@ -17,9 +21,16 @@ class RatesHttpRoutes[F[_]: Sync](rates: RatesProgram[F]) extends Http4sDsl[F] {
 
   private val httpRoutes: HttpRoutes[F] = HttpRoutes.of[F] {
     case GET -> Root :? FromQueryParam(from) +& ToQueryParam(to) =>
-      rates.get(RatesProgramProtocol.GetRatesRequest(from, to)).flatMap(Sync[F].fromEither).flatMap { rate =>
-        Ok(rate.asGetApiResponse)
-      }
+      rates
+        .get(RatesProgramProtocol.GetRatesRequest(from, to))
+        .flatMap(Sync[F].fromEither)
+        .flatMap { rate =>
+          Ok.apply(rate.asGetApiResponse)
+        }
+        .handleErrorWith {
+          case RateLookupFailed(msg) => InternalServerError(msg)
+          case NonFatal(error) => InternalServerError(error.getMessage)
+        }
   }
 
   val routes: HttpRoutes[F] = Router(
--- forex-mtl/src/test/scala/forex/http/rates/RatesHttpRoutesSpec.scala
+++ forex-mtl/src/test/scala/forex/http/rates/RatesHttpRoutesSpec.scala
@@ -10,7 +10,9 @@ import forex.programs.rates.Protocol._
 import forex.programs.rates.errors.Error
 import org.http4s.implicits._
 import org.http4s._
-import io.circe.Decoder
+import Converters._
+import forex.programs.rates.errors.Error.RateLookupFailed
+import org.http4s.Status.InternalServerError
 
 class RatesHttpRoutesSpec extends AnyFunSuite {
   test("return 200 OK when two valid currencies are provided") {
@@ -24,9 +26,34 @@ class RatesHttpRoutesSpec extends AnyFunSuite {
     val response = routes.routes.orNotFound.run(request).unsafeRunSync()
     val apiResponse = response.as[GetApiResponse].unsafeRunSync()
 
-    assert(apiResponse.from == rate.pair.from)
-    assert(apiResponse.to == rate.pair.to)
-    assert(apiResponse.price == rate.price)
-    assert(apiResponse.timestamp == rate.timestamp)
+    assert(apiResponse == rate.asGetApiResponse)
+  }
+
+  test("return 500 Internal Server Error when rates lookup fails with lookup error") {
+    val ratesProgram = new Algebra[IO] {
+      override def get(request: GetRatesRequest): IO[Either[Error, Rate]] =
+        IO(Left(RateLookupFailed("lookup error")))
+    }
+    val routes = new RatesHttpRoutes[IO](ratesProgram)
+    val request = Request[IO](method = Method.GET, uri = uri"/rates?from=USD&to=CAD")
+    val response = routes.routes.orNotFound.run(request).unsafeRunSync()
+    val body = response.as[String].unsafeRunSync()
+
+    assert(response.status == InternalServerError)
+    assert(body == "lookup error")
+  }
+
+  test("return 500 Internal Server Error when rates lookup fails with unhandled error") {
+    val ratesProgram = new Algebra[IO] {
+      override def get(request: GetRatesRequest): IO[Either[Error, Rate]] =
+        IO.raiseError(new Exception("unhandled error"))
+    }
+    val routes = new RatesHttpRoutes[IO](ratesProgram)
+    val request = Request[IO](method = Method.GET, uri = uri"/rates?from=USD&to=CAD")
+    val response = routes.routes.orNotFound.run(request).unsafeRunSync()
+    val body = response.as[String].unsafeRunSync()
+
+    assert(response.status == InternalServerError)
+    assert(body == "unhandled error")
   }
 }
